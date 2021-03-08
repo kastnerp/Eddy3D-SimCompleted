7b36c1df5528fe5ba52bfd5a741a6cdb0720e4d0
Patrick Kastner <patrick.kastner@gmail.com>
2021-03-08 10:50:41 -0500

    Update

--- check_sim_completed.bat
+++ check_sim_completed.bat
@@ -1,4 +1,4 @@
 call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
 call C:\Users\pkastner\Anaconda3\Scripts\activate.bat C:\Users\pkastner\Anaconda3
-python check_sim_completed.py
+python check_sim_completed.py | tee.bat status.txt 1
 PAUSE
