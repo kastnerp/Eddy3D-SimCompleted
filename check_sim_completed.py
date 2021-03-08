fe7a5dcecfa2582dff108b9b6a53dd2b4b0dc076
Patrick Kastner <patrick.kastner@gmail.com>
2021-03-07 19:40:06 -0500

    Update check_sim_completed.py

--- check_sim_completed.py
+++ check_sim_completed.py
@@ -4,22 +4,18 @@ import re
 from termcolor import colored
 import colorama
 
-
 colorama.init()
 
 cwd = Path.cwd()
 
 # List files to see what is mounted
-cwd = r"E:\CampusSmallPatch_BS5"
-
-control_dicts = []
-end_times = []
+#cwd = r"E:\CampusSmallPatch_BS5"
 
 
 def print_progress(cnt):
     progress = int(last_iters[cnt]) / int(end_times[cnt]) * 100
     if progress > 0:
-        print(str(sim_dirs[cnt]), "-", colored(str(round(progress,1)) + "% done", "cyan"))
+        print(str(sim_dirs[cnt]), "-", colored(str(round(progress, 1)) + "% done", "cyan"))
     else:
         print(str(sim_dirs[cnt]), "-", colored("Not Started", "white"))
 
@@ -33,36 +29,51 @@ def get_all_subdirs(path):
     return l
 
 
-for root, dirs, files in os.walk(cwd):
-    for file in files:
-        if file.startswith("controlDict") and "mesh" not in str(root):
-            p = os.path.join(root, file)
-            control_dicts.append(p)
-            # print(p)
-            try:
-                fp = open(p)
-                for cnt, line in enumerate(fp):
-                    if "endTime" in str(line):
-                        words = re.split('\s+', line)
-                        if words[2].split(';')[0].isnumeric():
-                            end_times.append(words[2].split(';')[0])
-
-            finally:
-                fp.close()
+def get_control_dicts(cwd):
+    control_dicts = []
+    end_times = []
+    for root, dirs, files in os.walk(cwd):
 
 
-sim_dirs = []
-last_iters = []
-for d in control_dicts:
-    path = os.sep.join(d.split(os.sep)[0:-2])
-    sim_dirs.append(path)
-    l = get_all_subdirs(path)
-    last_iter = 0
-    for el in l:
-        if el.isnumeric():
-            if int(el) > int(last_iter):
-                last_iter = el
-    last_iters.append(last_iter)
+
+        for file in files:
+            if file.startswith("controlDict") and "mesh" not in str(root):
+                p = os.path.join(root, file)
+                control_dicts.append(p)
+                # print(p)
+                try:
+                    fp = open(p)
+                    for cnt, line in enumerate(fp):
+                        if "endTime" in str(line):
+                            words = re.split('\s+', line)
+                            if words[2].split(';')[0].isnumeric():
+                                end_times.append(words[2].split(';')[0])
+
+                finally:
+                    fp.close()
+
+    return control_dicts, end_times
+
+
+def get_last_iterations(control_dicts):
+    sim_dirs = []
+    last_iters = []
+
+    for d in control_dicts:
+        path = os.sep.join(d.split(os.sep)[0:-2])
+        sim_dirs.append(path)
+        l = get_all_subdirs(path)
+        last_iter = 0
+        for el in l:
+            if el.isnumeric():
+                if int(el) > int(last_iter):
+                    last_iter = el
+        last_iters.append(last_iter)
+
+    return sim_dirs, last_iters
+
+
+
 
 def check_if_converged(dir, cnt):
     # Check if converged
@@ -71,17 +82,18 @@ def check_if_converged(dir, cnt):
     logfile = dir / Path("log")
     if logfile.is_file():
         fp = open(logfile)
+        lines = fp.readlines()
         try:
-            for _, line in enumerate(fp):
+            for line in lines:
                 if "SIMPLE solution converged in" in str(line):
                     converged = True
-                    break
-                if "job aborted:" in str(line):
+                    #break
+                elif "job aborted:" in str(line):
                     crashed = True
-                    break
-                if "Exec   : simpleFoam":
+                    #break
+                elif "Exec   : simpleFoam":
                     crashed = False
-                    break
+                    #break
 
             if converged == True:
                 print(str(dir), "-", colored("Done", "green"))
@@ -96,6 +108,14 @@ def check_if_converged(dir, cnt):
 
     else:
         print_progress(cnt)
+
+
+
+control_dicts, end_times = get_control_dicts(cwd)
+
+sim_dirs ,  last_iters = get_last_iterations(control_dicts)
+
+
 
 
 for cnt, dir in enumerate(sim_dirs):
@@ -119,10 +139,3 @@ for cnt, dir in enumerate(sim_dirs):
     except:
 
         check_if_converged(sim_dirs[cnt], cnt)
-
-
-
-
-
-
-
