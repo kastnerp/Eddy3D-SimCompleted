0831c51570ebd620faef2aa6a3380ea31e1d3757
Patrick Kastner <patrick.kastner@gmail.com>
2024-03-24 17:08:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.3',  # Required
+    version='0.2.4',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -180,7 +180,7 @@ setup(
     # executes the function `main` from this package when invoked:
     entry_points={
         'console_scripts': [
-            'pyeddy3d=pyeddy3d.main:main',
+            'pyeddy3d=pyeddy3d.cli:analyze_cwd',
         ],
     },
 
