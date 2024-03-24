7fcb3fad7df760fe457d7d68ffd02494b2e43900
Patrick Kastner <patrick.kastner@gmail.com>
2024-03-24 16:28:59 -0400

    .

--- pyeddy3d/__main__.py
+++ pyeddy3d/__main__.py
@@ -1,14 +1,11 @@
 from pyeddy3d import SimCompleted
 
-# pyeddy3d/__main__.py
+
 
 def main():
     print("Analyzing current directory with pyEddy3D...")
     s = SimCompleted.Simulation()
     s.analyze()
 
-
-
-
-
-
+if __name__ == '__main__':
+    main()
