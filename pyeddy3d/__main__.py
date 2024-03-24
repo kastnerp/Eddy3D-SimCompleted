c84bd86615d9ce2446be16ab29d838370e707dd1
Patrick Kastner <patrick.kastner@gmail.com>
2024-03-24 16:44:35 -0400

    Create __main__.py

---
+++ pyeddy3d/__main__.py
@@ -0,0 +1,14 @@
+from pyeddy3d import SimCompleted
+
+# pyeddy3d/__main__.py
+
+def analyze_cwd():
+    print("Analyzing current directory with pyEddy3D...")
+    s = SimCompleted.Simulation()
+    s.analyze()
+
+
+
+
+
+
